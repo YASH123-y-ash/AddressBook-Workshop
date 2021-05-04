c8dadea8dfdbccc791fc3b5c7c6c672cab072794
Yash-y-ash <wardhan696@gmail.com>
2021-05-05 03:17:01 +0530

    Adding person in addressbook

--- main/java/com/addressbook/AddressBook.java
+++ main/java/com/addressbook/AddressBook.java
@@ -5,49 +5,20 @@ import java.util.Scanner;
 
 public class AddressBook {
 
-    private ArrayList<Person> data=new ArrayList<Person>();
-
-    //method to read string value from user
-    public String readString(String displayMessage){
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println(displayMessage);
-        String input=sc.nextLine();
-
-        return input;
-    }
-
-    //method to read long data
-    public long readLong(String displayMessage){
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println(displayMessage);
-        long input=sc.nextLong();
-
-        return input;
-    }
-
-    //method to add person to address book
-    public boolean addPerson(){
-        String firstName=readString("Enter Person's first name: ");
-        String lastName = readString("Enter person's last name");
-        String city=readString("Enter where he/she lives first enter City: ");
-        String state=readString("Now, enter State: ");
-        String address=readString("Now, enter full address: ");
-        String email=readString("Now, enter email: ");
-        String zipCode=readString("At last enter, area's Zip Code: ");
-        long phoneNumber=readLong("Enter his/her phone number: ");
-        Person newEntry=new Person(firstName, lastName, city, state, address, email, zipCode, phoneNumber);
-
-        this.data.add(newEntry);
-        System.out.println(firstName+" added successfully to address book.");
-        boolean added = this.data.add(newEntry);
+    ArrayList<Person> data = new ArrayList<>();
+    public boolean addPerson()
+    {
+        Person person = new Person("Yash", "Wardhan", "purnea",
+                "Bihar", "law collage road", "wardhan@gmail.com",
+                "567474", 8834543444l);
+        this.data.add(person);
+        boolean added = this.data.add(person);
         return added;
     }
 
     public static void main(String[] args) {
-        AddressBook addressBook = new AddressBook();
-        addressBook.addPerson();
-
+        Person person = new Person("Yash", "Wardhan", "purnea",
+                "Bihar", "law collage road", "wardhan@gmail.com",
+                "567474", 8834543444l);
     }
 }
